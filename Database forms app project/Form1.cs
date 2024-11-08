18a738da16a57972b03683f1f8844a27c853a061
Laura Hatherly <18LHatherly@heckgrammar.co.uk>
2024-11-08 14:01:45 +0000

    .

--- Database forms app project/Form1.cs
+++ Database forms app project/Form1.cs
@@ -2,9 +2,35 @@ namespace Database_forms_app_project
 {
     public partial class Form1 : Form
     {
+        bool form2showing = false;
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void bntForm2_Click(object sender, EventArgs e)
+        {
+            if (form2showing){
+                MessageBox.Show("Already open");
+            }else
+            {
+                Form form2 = new Form2();
+                form2.Show();
+                form2showing = true;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Button bntForm2 = new Button()
+            {
+                Location = new Point(60, 50),
+            BackColor = Color.Thistle,
+                Text = "Insert values into table.",
+            };
+            bntForm2.Size = new Size(200, 50);
+            Controls.Add(bntForm2);
+            bntForm2.Click += bntForm2_Click;
         }
     }
 }
---
+++ Database forms app project/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace Database_forms_app_project
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            ComboBox tables = new ComboBox();
+            tables.Location = new Point(10, 10);
+            tables.DropDownStyle = ComboBoxStyle.DropDownList;
+            tables.Name = "tablesList";
+            Controls.Add(tables);
+
+            MySqlConnection con = new MySqlConnection("server=192.168.35.248;uid=18LHatherly;pwd=4fJyYyWU;database=18LHatherly _Bookshop");
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = con;
+            string SQL = "SHOW TABLES";
+            cmd.CommandText = SQL;
+            con.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    tables.Items.Add(reader[i].ToString());
+                }
+                
+            }
+            con.Close();
+        }
+    }
+}
